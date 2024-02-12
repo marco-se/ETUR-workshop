a8c737f10be582945711b7367651a60035670cad
Marco Bielesch <Marco.Bielesch@plan-b-gmbh.com>
2024-02-12 16:05:41 +0100

    Adds schemas

--- src/services/customer-number-server/index.js
+++ src/services/customer-number-server/index.js
@@ -1,4 +1,4 @@
-import { GetAllCustomers, CreateCustomer, GetCustomer, RemoveCustomer } from "./customer.js";
+import { GetAllCustomers, CreateCustomer, GetCustomer, RemoveCustomer, ValidateCustomerNumber } from "./customer.js";
 /*
 const message = "Hello World";
 console.log(message);
@@ -8,34 +8,92 @@ const index = Math.round(Math.random() * 2)
 console.log("Removed:", RemoveCustomer("ETUR-CN-" + index));
 console.log("All:", GetAllCustomer());
 */
-
 import Fastify from 'fastify'
 const fastify = Fastify({
   logger: true
 })
+const customerInputOutputSchema = {
+    schema: {
+      body: {
+        type: 'object',
+        properties: {
+          name: { type: 'string' },
+          email: { type: 'string' },
+          phone: { type: 'string' },
+        },
+        required: ['name', 'email', 'phone']
+      },
+      response: {
+        200: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              email: { type: 'string' },
+              phone: { type: 'string' },
+            }
+        }
+      }
+    }
+}
+const customerOutputSchema = {
+    schema: {
+      response: {
+        200: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              email: { type: 'string' },
+              phone: { type: 'string' },
+            }
+        }
+      }
+    }
+}
+const customersOutputSchema = {
+    schema: {
+      response: {
+        200: {
+          type: 'array',
+          items: {
+            type: 'object',
+            properties: {
+              id: { type: 'string' },
+              name: { type: 'string' },
+              email: { type: 'string' },
+              phone: { type: 'string' },
+            }
+          }
+        }
+      }
+    }
+}
 
 // Get all customers
-fastify.get('/', async function handler (request, reply) {
+fastify.get('/', customersOutputSchema, async function handler (request, reply) {
   return GetAllCustomers()
 })
 //Get all customers
-fastify.get('/customers', async function handler (request, reply) {
+fastify.get('/customers', customersOutputSchema, async function handler (request, reply) {
     return GetAllCustomers()
 })
 //Get a customer by id
-fastify.get('/customers/:id', async function handler (request, reply) {
+fastify.get('/customers/:id', customerOutputSchema, async function handler (request, reply) {
     return GetCustomer(request.params.id)
 })
 //Create a customer
-fastify.post('/customers', async function handler (request, reply) {
+fastify.post('/customers', customerInputOutputSchema, async function handler (request, reply) {
     return CreateCustomer(request.body.name, request.body.email, request.body.phone)
 })
 //Remove a customer
-fastify.delete('/customers/:id', async function handler (request, reply) {
+fastify.delete('/customers/:id', customersOutputSchema, async function handler (request, reply) {
     return RemoveCustomer(request.params.id)
 })
 //Validate a customer number
-
+fastify.get('/validate/:id', async function handler (request, reply) {
+    return ValidateCustomerNumber(request.params.id)
+})
 
 // Run the server!
 try {

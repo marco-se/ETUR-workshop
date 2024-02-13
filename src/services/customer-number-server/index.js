267c56f327955b4f83d11d0597c881e3b8ee3b38
Marco Bielesch <Marco.Bielesch@plan-b-gmbh.com>
2024-02-13 08:43:56 +0100

    Adds fastify cors

--- src/services/customer-number-server/index.js
+++ src/services/customer-number-server/index.js
@@ -1,14 +1,10 @@
 import { GetAllCustomers, CreateCustomer, GetCustomer, RemoveCustomer, ValidateCustomerNumber } from "./customer.js";
-/*
-const message = "Hello World";
-console.log(message);
-console.log("Created:", CreateCustomer("Tom", "tom@example.com", "1234567890"))
-console.log("Created:", CreateCustomer("Otto", "otto@example.com", "2345678901"))
-const index = Math.round(Math.random() * 2)
-console.log("Removed:", RemoveCustomer("ETUR-CN-" + index));
-console.log("All:", GetAllCustomer());
-*/
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+fastify.register(cors, {
+  origin: '*'
+});
 const fastify = Fastify({
   logger: true
 })
